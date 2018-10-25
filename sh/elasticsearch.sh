2176dad2b3082f236560daa5cba102c091770a13
Егор Артемов <artemov93@gmail.com>
2018-10-25 19:49:29 +0300

    Update elasticsearch.sh

--- sh/elasticsearch.sh
+++ sh/elasticsearch.sh
@@ -4,7 +4,13 @@ echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | tee -a /e
 apt-get update && apt-get install -y openjdk-8-jdk apt-transport-https
 java -version
 dpkg --configure -a
-apt-get install -y elasticsearch
+apt-get install -y elasticsearch kibana
 echo 'network.host: '${IP} >> /etc/elasticsearch/elasticsearch.yml
+echo 'http.port: 9200' >> /etc/elasticsearch/elasticsearch.yml
+echo 'server.host: '${IP} >> /etc/kibana/kibana.yml
+echo 'server.port: 5601' >> /etc/kibana/kibana.yml
+echo 'server.name: '${HOSTNAME} >> /etc/kibana/kibana.yml
+echo 'elasticsearch.url: http://'${IP}':9200' >> /etc/kibana/kibana.yml
+echo 'logging.verbose: true' >> /etc/kibana/kibana.yml
 /bin/systemctl daemon-reload
 /bin/systemctl enable elasticsearch.service
