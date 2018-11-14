fd794612156e66f1b66b567c314244403dea72e2
Егор Артемов <artemov93@gmail.com>
2018-11-14 13:06:40 +0300

    Create freeipa-client.sh

---
+++ sh/freeipa-client.sh
@@ -0,0 +1,12 @@
+export DEBIAN_FRONTEND=noninteractive
+DEBIAN_VER='stretch'
+wget -qO - http://apt.numeezy.fr/numeezy.asc | apt-key add -
+echo -e 'deb http://apt.numeezy.fr  '${DEBIAN_VER}' main' >> /etc/apt/sources.list.d/ipa.list
+apt-get update && apt-get install -y freeipa-client
+mkdir -p /etc/pki/nssdb
+certutil -N -d /etc/pki/nssdb
+mkdir -p /var/run/ipa
+ipa-client-install
+echo 'session required pam_mkhomedir.so' >> /etc/pam.d/common-session
+systemctl restart sssd
+systemctl restart ssh
